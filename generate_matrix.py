fec132e0811f8c4dba314190c607c0cd2d9b214a
piernik <wojtek@surowka.com>
2019-05-21 12:50:01 +0200

    zamysł modułu

---
+++ generate_matrix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import cv2 
+def generate_matrix(cordList):
+	"""
+	takes python list as a cordynates:
+		left top corner 
+		right top corner 
+		left bottom corner 
+		right bottom corner 
+		middle at the beginning of file 
+		middle at the end of file
+	returns:
+		list of transforation matrixies to achive imput rectangle of:
+			width:	1024
+			hight:	768
+	"""
+	pass
